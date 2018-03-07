9bcc5966ddbcebfa7724efab43740f468a341f99
Roy Åne Sylthe <royaane@gmail.com>
2018-03-07 13:12:14 +0100

    Solved oppgave2.. I think.

--- Oppgave2/Oppgave2.py
+++ Oppgave2/Oppgave2.py
@@ -6,12 +6,16 @@ from scipy.sparse import csr_matrix
 
 def make_a(n):
     e = sp.ones(n)
-    A = spdiags([e, -4*e, 6*e, -4*e, e], [-2, -1, 0, 1, 2], n, n)
+    A = spdiags([e, -4 * e, 6 * e, -4 * e, e], [-2, -1, 0, 1, 2], n, n)
     A = lil_matrix(A)
-    B = csr_matrix([[1, 2, 3, 4],  # Example matrix
-                   [5, 6, 7, 8],
-                   [9, 10, 11, 12]])
+    B = csr_matrix([[16, -9, 8 / 3, -1 / 4],
+                    [16 / 17, -60 / 17, 72 / 17, -28 / 17],
+                    [-12 / 17, 96 / 17, -156 / 17, 72 / 17]])
     A[0, 0:4] = B[0, :]
-    A[n-2, n-4:n] = B[1, :]
-    A[n-1, n-4:n] = B[2, :]
+    A[n - 2, n - 4:n] = B[1, :]
+    A[n - 1, n - 4:n] = B[2, :]
     return A
+
+
+# print(make_a(6).toarray(), file=open('oppgave2.txt', 'w'))
+print(make_a(6))
