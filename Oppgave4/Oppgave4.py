8c71ea75009b03c6f1bd1597a508cc53689411ce
Kristoffer <kristoffer.arn@gmail.com>
2018-03-19 12:52:56 +0100

    JAHADA

--- Oppgave4/Oppgave4.py
+++ Oppgave4/Oppgave4.py
@@ -37,14 +37,16 @@ print(Ay_e)
 # Oppgave 4d
 y4_e = csr_matrix([f(x)/(E*I) for x in range(0, n)]).T
 FE = norm(abs(y4_e - Ay_e), 1)  # Forward Error
-RFE = FE / np.max(abs(y4_e))  # Relative Forward Error
-BE = 2**-52  # Backwards Error (Given in assignment)
-# RBE =
+RFE = norm(abs(Ay_e - y4_e), sp.inf) / norm(abs(y4_e), sp.inf)  # Relative Forward Error
+RBE = 2**-52  # Relative Backwards Error (Given in assignment)
+ErrMag = RFE/RBE  # Error Magnification Factor
 
 print("\n\nOppgave 4d:")
-print("Backward error: ", BE, " or ", BE / (2**-52), " machineeps")
 print("Forward error: ", FE, " or ", FE / (2**-52), " machineeps")
-print("Relative Forward error: ", RFE)
+print("Relative Forward error: ", RFE, " or ", RFE / (2**-52), " machineeps")
+print("Relative Backward error: ", RBE, " or ", RBE / (2**-52), " machineeps")
+print("Error Magnification Factor: ", ErrMag)
+print("Condition Number of A: ", condA)
 
 
 # Oppgave 4e
@@ -53,3 +55,4 @@ FE = norm(abs(y_c-y_e), 1)
 
 print("\n\nOppgave 4e:")
 print("Forward error: ", FE / (2**-52), " machineeps")
+
